41a8ce1d51e4ef872f2abd8a2f182a0834b553fc
areotwister <areotwister@gmail.com>
2021-11-05 00:04:05 +0100

    none

---
+++ Betriebssysteme_Tutorate/Tutorat_2_Hardware/aufgabe_3/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+int value;
+FILE *fptr = fopen("myfile.txt","w");
+for(int i = 0; i < 2500; i++)
+{
+fprintf(fptr,"%d ",i);
+}
+fclose(fptr);
+fptr = fopen("myfile.txt", "r");
+for(int j = 0; j < 2500; j++)
+{
+fscanf(fptr, "%d ", &value);
+}
+fclose(fptr);
+return 42;
+}
